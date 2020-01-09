81685d059cb0fd82180560653aa50e97afbed9fd
Paras Jain <parasjain.jain@outlook.com>
2020-01-09 15:58:41 +0530

    Merge branch 'master' of https://github.com/subhash0x/school-Darshika
    # Conflicts:
    #	main/migrations/0001_initial.py
    #	main/react/src/containers/Login.js
    #	main/react/src/containers/LoginForm.js
    #	main/react/src/routes.js

--- main/react/src/containers/LoginForm.js
+++ main/react/src/containers/LoginForm.js
@@ -29,49 +29,51 @@ class LoginForm extends Component {
     const { getFieldDecorator } = this.props.form;
     return (
 
-        <Col span={20}  style={{ display: 'block',  padding: '10px'}}>
+        <Col sm={20}  style={{ display: 'block',  padding: '10px'}}>
 
-            <div className="container" style={{position:'relative', marginLeft:'30px', backgroundColor:'rgb(220,220,220)', padding: '10px', borderRadius:'5px'}}>
+            <div className="container" style={{position:'relative', margin:'50px', backgroundColor:'rgb(240,240,240)', padding: '30px', borderRadius:'5px'}}>
 
-                <Form onSubmit={this.handleSubmit} className="login-form">
-                    <Form.Item>
-                      {getFieldDecorator('email', {
-                        rules: [{ required: true, message: 'Please input your email!' }],
-                      })(
-                        <Input
-                            id='email'
-                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                            placeholder="Email"
-                        />,
-                      )}
-                    </Form.Item>
-                    <Form.Item>
-                      {getFieldDecorator('password', {
-                        rules: [{ required: true, message: 'Please input your Password!' }],
-                      })(
-                        <Input
-                          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                          type="password"
-                          id='password'
-                          placeholder="Password"
-                        />,
-                      )}
-                    </Form.Item>
-                    <Form.Item>
-                      {getFieldDecorator('remember', {
-                        valuePropName: 'checked',
-                        initialValue: true,
-                      })(<Checkbox>Remember me</Checkbox>)}
-                      <a className="login-form-forgot" href="">
-                        Forgot password
-                      </a>
-                      <Button type="primary" htmlType="submit" className="login-form-button">
-                        Log in
-                      </Button>
-                      Or <a href="">register now!</a>
-                    </Form.Item>
-                </Form>
-            </div>
+            <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator('email', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Email"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />,
+          )}
+        </Form.Item>
+
+        <Form.Item>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(<Checkbox>Remember me</Checkbox>)}
+          <br></br>
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+             <br></br>
+          <Button type="primary" htmlType="submit" className="login-form-button" block>
+            Log in
+          </Button>
+            <br></br>
+          <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}> Or <a href="">register now!</a></div>
+        </Form.Item>
+      </Form>
+                </div>
         </Col>
 
     );
--- main/react/src/routes.js
+++ main/react/src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Login from './containers/Login';
 import SchoolsList from './containers/SchoolsList'
+import signup from './containers/signup';
 import TestComponent from './containers/TestComponent';
 
 
@@ -12,6 +13,7 @@ const BaseRouter = () => (
 		<Route exact path={`${BASE_PATH}/schools`} component={SchoolsList} />
 		<Route exact path={`${BASE_PATH}/login`} component={Login} />
 		<Route exact path={`${BASE_PATH}/test`} component={TestComponent} />
+		<Route exact path={`${BASE_PATH}/signup`} component={signup} />
 	</div>
 );
 
