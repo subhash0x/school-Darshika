9d9ec6e4f9407127c327df808350772a23b882fc
parasjain1 <parasjain.jain@outlook.com>
2020-01-12 04:06:06 +0530

    sign up complete

--- main/react/src/containers/signup.js
+++ main/react/src/containers/signup.js
@@ -55,13 +55,11 @@ class signup extends React.Component {
           if (!err) {
               APIClient.signup(values).then((response) => {
                   message.info("Signed up successfully!");
-                  window.location = "/login";
+                  window.location = "/app/login";
               }).catch((error) => {
                   message.error("Couldn't sign up. Please try again!");
                   console.log(error);
-
-                  ;
-                  //console.log('Received values of form: ', values);
+                  console.log('Received values of form: ', values);
               });
           }
       });
@@ -146,9 +144,15 @@ class signup extends React.Component {
  <div className="container" style={{position:'relative', margin:'50px', backgroundColor:'rgb(240,240,240)', padding: '30px', borderRadius:'5px'}}>
 
         <Form {...formItemLayout} onSubmit={this.handleSubmit}>
-             <Form.Item label="Name">
-
-          <Input />
+          <Form.Item label="Name">
+          {getFieldDecorator('name', {
+            rules: [
+              {
+                required: true,
+                message: 'Please input your full name!',
+              },
+            ],
+          })(<Input />)}
         </Form.Item>
 
         <Form.Item label="E-mail">
@@ -202,7 +206,7 @@ class signup extends React.Component {
 
 
  <Form.Item label="Adhar Number">
-          {getFieldDecorator('Adhar', {
+          {getFieldDecorator('aadhaar', {
             rules: [{ required: true, message: 'Please input your Adhar number!' }],
           })(<Input maxLength={12} style={{ width: '100%' }} />)}
         </Form.Item>
@@ -210,7 +214,7 @@ class signup extends React.Component {
 
 
         <Form.Item {...tailFormItemLayout}>
-          {getFieldDecorator('agreement', {
+          {getFieldDecorator('is_ngo', {
             valuePropName: 'checked',
           })(
             <Checkbox>
